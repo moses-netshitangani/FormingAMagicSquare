9b435beee6ff361093f260f87249e5bace0c191b
Moses <netshitanganimoses@gmail.com>
2024-05-23 22:47:49 +0200

    idea

---
+++ Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'formingMagicSquare' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY s as parameter.
+     */
+
+    public static int formingMagicSquare(List<List<Integer>> s) {
+        // sum matrix by row, diagonal and then column, from [0][0]
+        // if a sum is less than 15, note the difference and direction
+        // if there is only one direction, sum row starting from [1][0]
+        // if there is still just one, sum columns starting from [0][1]
+
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> s = new ArrayList<>();
+
+        IntStream.range(0, 3).forEach(i -> {
+            try {
+                s.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.formingMagicSquare(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
